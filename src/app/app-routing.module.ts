1cbc0f368006f5589ea1691787227df0d31b1057
Toni Pigolotti <pigolottiantonino@gmail.com>
2020-11-06 16:03:43 +0100

    modifiche ven 6 nov.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ConfermaDatiComponent } from './conferma-dati/conferma-dati.component';
+import { QuestionarioComponent } from './questionario/questionario.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'questionario', component : QuestionarioComponent},
+  {path:'form', component : ConfermaDatiComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
