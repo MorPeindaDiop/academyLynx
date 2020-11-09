9c10f593d2ae3e2e3c7f606924fcefbfb40fe0c6
Toni Pigolotti <pigolottiantonino@gmail.com>
2020-11-09 14:30:56 +0100

    risultato

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ConfermaDatiComponent } from './conferma-dati/conferma-dati.component';
 import { QuestionarioComponent } from './questionario/questionario.component';
+import { RisultatoComponent } from './risultato/risultato.component';
 
 
 const routes: Routes = [
   {path:'questionario', component : QuestionarioComponent},
   {path:'form', component : ConfermaDatiComponent},
+  {path:'risultato', component : RisultatoComponent},
   {path: '', redirectTo: '/form', pathMatch: 'full' },
 ];
 
