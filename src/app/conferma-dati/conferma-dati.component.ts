6c9ab18be26de31372ef17e23beb7bc42527bf36
MorPeindaDiop <61980976+MorPeindaDiop@users.noreply.github.com>
2020-11-09 16:00:09 +0100

    multiselect

--- src/app/conferma-dati/conferma-dati.component.ts
+++ src/app/conferma-dati/conferma-dati.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
 
 @Component({
   selector: 'app-conferma-dati',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./conferma-dati.component.scss']
 })
 export class ConfermaDatiComponent implements OnInit {
+  dropdownList = [];
+  selectedItems = [];
+  dropdownSettings:IDropdownSettings;
+  isDropdown=false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.isDropdown=true;
+
+    this.dropdownList = [
+      { item_id: 1, item_text: 'Roma' },
+      { item_id: 2, item_text: 'Milano' },
+      { item_id: 3, item_text: 'Venezia' },
+      { item_id: 4, item_text: 'Bologna' },
+      { item_id: 5, item_text: 'Lecco' }
+    ];
+    
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 5,
+      allowSearchFilter: true
+    };
+  }
+  onItemSelect(item: any) {
+    console.log(item);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
   }
 
 }
