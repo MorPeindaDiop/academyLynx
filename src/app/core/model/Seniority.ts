70317d54a1fe1f485c7a5d27318e54251711a356
MorPeindaDiop <61980976+MorPeindaDiop@users.noreply.github.com>
2020-11-16 14:30:10 +0100

    provaMaster

--- src/app/core/model/Seniority.ts
+++ src/app/core/model/Seniority.ts
@@ -3,8 +3,8 @@ import { Time } from '@angular/common';
 export interface Seniority {
     id: number;
     description: string;
-    minDifficulty: number;
-    maxDifficulty: number;
+    minDifficulty?: number;
+    maxDifficulty?: number;
     creation_time?: Date;
     update_time?: Date;
     creationUser?: string;
--- src/app/features/conferma-dati/main/conferma-dati.component.ts
+++ src/app/features/conferma-dati/main/conferma-dati.component.ts
@@ -14,66 +14,67 @@ import { Skill } from 'src/app/core/model/Skill';
   styleUrls: ['./conferma-dati.component.scss']
 })
 export class ConfermaDatiComponent implements OnInit {
-  dropdownList = [];
+  dropdownListSkill = [];
+  dropdownListSeniority = [];
   selectedItems = [];
   dropdownSettings: IDropdownSettings;
   isDropdown = false;
   skills : Observable < Response > ;
   dbSkill = [];
+  senioritys : Observable < Response > ;
+  dbSeniority = [];
   constructor(private http: HttpCommunicationsService) { }
 
   ngOnInit(): void {
-    this.isDropdown = true;
-    
+   // this.isDropdown = true;
+  
+
     this.skills=this.http.retrieveGetCall< Response >("skill/findAll");
     console.log(this.skills);
     this.skills.pipe().subscribe((skill) =>
-      skill.result.forEach((item)=> this.dropdownList.push({ item_id: item.id, item_text: item.description })),
-      
+      skill.result.forEach((item)=> this.dropdownListSkill.push({ item_id: item.id, item_text: item.description })),
       );
 
-    console.log(this.dropdownList);
+    console.log(this.dropdownListSkill);
+      
+    this.senioritys=this.http.retrieveGetCall< Response >("seniority/findAll");
+    console.log(this.senioritys);
+    this.senioritys.pipe().subscribe((seniority) =>
+      seniority.result.forEach((item)=> this.dropdownListSeniority.push({ item_id: item.id, item_text: item.description })),
+      );
 
-    // this.dropdownList = [
-    //   { item_id: 1, item_text: 'Roma' },
-    //   { item_id: 2, item_text: 'Milano' },
-    //   { item_id: 3, item_text: 'Venezia' },
-    //   { item_id: 4, item_text: 'Bologna' },
-    //   { item_id: 5, item_text: 'Lecco' }
-    // ];
+    console.log(this.dropdownListSeniority);
     
-    this.dropdownSettings = {
-      singleSelection: false,
-      idField: 'item_id',
-      textField: 'item_text',
-      selectAllText: 'Select All',
-      unSelectAllText: 'UnSelect All',
-      itemsShowLimit: 5,
-      allowSearchFilter: true
-    };
-  }
-  onItemSelect(item: any) {
-    console.log(item);
-  }
-  onSelectAll(items: any) {
-    console.log(items);
   }
   
-  getPosts() {
-    this.skills=this.http.retrieveGetCall< Response >("skill/findAll");
-    console.log(this.skills);
-    this.skills.pipe().subscribe((skill) =>
-      skill.result.forEach((item)=> this.dbSkill.push(item)),
+  // getPost() {
+  //   this.skills=this.http.retrieveGetCall< Response >("skill/findAll");
+  //   console.log(this.skills);
+  //   this.skills.pipe().subscribe((skill) =>
+  //     skill.result.forEach((item)=> this.dbSkill.push(item)),
       
-      );
+  //     );
 
-      console.log(this.dbSkill);
-    // this.skills.pipe(switchMap( async (skills) => skills.forEach((skill) => this.candidateAnswer.push("ciao") )));
-    // console.log(this.candidateAnswer);
-    //.subscribe(todo => { this.todo = todo; })
-    //this.posts = this.http.get(this.ROOT_URL );s
+  //     console.log(this.dbSkill);
 
-    return JSON.stringify(this.dbSkill);
-  }
-  
+      
+  //   // this.skills.pipe(switchMap( async (skills) => skills.forEach((skill) => this.candidateAnswer.push("ciao") )));
+  //   // console.log(this.candidateAnswer);
+  //   //.subscribe(todo => { this.todo = todo; })
+  //   //this.posts = this.http.get(this.ROOT_URL );s
+
+  //   return JSON.stringify(this.dbSkill);
+    
+  // }
+  // getPosts1(){
+  //   this.senioritys=this.http.retrieveGetCall< Response >("seniority/findAll");
+  //   console.log(this.senioritys);
+  //   this.senioritys.pipe().subscribe((seniority) =>
+  //     seniority.result.forEach((item)=> this.dbSeniority.push(item)),
+      
+  //     );
+
+  //     console.log(this.dbSeniority);
+  //     return JSON.stringify(this.dbSeniority);
+  // }
 }
