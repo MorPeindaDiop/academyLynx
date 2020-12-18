16ac51b4cf031b42f2071876893f00a986e05208
AndreaLoNivuro <a.lonivuro@live.it>
2020-12-18 15:32:41 +0100

    hola

--- src/app/features/risultato/services/risultato.service.ts
+++ src/app/features/risultato/services/risultato.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { retrieveAllCandidateAnswers, retrieveCandidateAnswersByIdCandidate } from 'src/app/redux/candidate-answer/candidate-answer.actions';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RisultatoService {
 
-  constructor() { }
+  constructor(private store: Store) { }
   
+  retrieveCandidateAnswersByIdCandidate(idCandidate: number) {
+    this.store.dispatch(retrieveCandidateAnswersByIdCandidate({idCandidate}));
+  }
+
 }
